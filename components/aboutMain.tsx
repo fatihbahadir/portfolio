8e05dd0ccd8ae5129002324e392eaeaff0094059
fatihbahadir <hunkarhyme@gmail.com>
2024-08-07 16:29:20 +0300

    feat(about):main section completed

---
+++ components/aboutMain.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { bebas_neue } from "@/lib/fonts";
+import React from "react";
+import Buttons from "./buttons";
+import { FaDownload, FaExpand } from "react-icons/fa";
+import Image from "next/image";
+
+const AboutMain = () => {
+  return (
+    <section className="h-screen max-w-screen flex items-center justify-center -mt-[110px] ">
+        <div className="flex lg:flex-row flex-col items-start w-full justify-between lg:justify-between gap-6 lg:gap-0" >
+        <h2
+          className={`${bebas_neue.className} lg:text-[90px] xl:text-[101px] text-6xl leading-[90%] `}
+        >
+          ABOUT ME
+        </h2>
+
+      <div className="flex flex-col gap-5 max-w-[700px]">
+        <h5 className="text-3xl font-[500] leading-[140%]">
+          I am a front-end developer based in Istanbul, Currently 4th-year
+          Software Engineering student
+        </h5>
+        <p className="text-lg text-off-white leading-[150%] font-light">
+          I'm currently a fourth-year Software Enginering student at Maltepe
+          University in Istanbul, where I'm diving into the world of Fullstack
+          Development. When I was a child, I used to lose track of time while
+          playing computer games; now, I find myself losing track of time while
+          coding. I'm constantly driven by the pursuit of self-improvement and
+          the quest to tackle the most exciting challenges in the field.{" "}
+        </p>
+        <div>
+          <Buttons
+            text="SEE RESUME"
+            onClick={() => {
+              console.log("hello world");
+            }}
+            Icon={FaExpand}
+          />
+        </div>
+      </div>
+        </div>
+    </section>
+  );
+};
+
+export default AboutMain;
--- components/hero.tsx
+++ components/hero.tsx
@@ -1,15 +1,22 @@
+'use client';
+
 import { bebas_neue } from "@/lib/fonts";
 import Image from "next/image";
 import React from "react";
 import { BsArrowUpRight } from "react-icons/bs";
 import { FaGithub, FaLinkedin, FaLinkedinIn } from "react-icons/fa";
 import { FaArrowUpRightDots } from "react-icons/fa6";
+import Buttons from "./buttons";
+import { useRouter } from 'next/navigation'
+import * as NProgress from "nprogress";
+
 
 const Hero = () => {
   const text = "FATIH BAHADIR";
-  
+  const router = useRouter()
+
   return (
-    <section className="w-full h-screen flex lg:flex-row flex-col items-center lg:justify-between lg:-mt-[110px]  gap-6 lg:gap-0">
+    <section className="max-w-screen h-screen flex lg:flex-row flex-col items-center lg:justify-between lg:-mt-[110px]  gap-6 lg:gap-0">
       <div className="flex flex-col text-start justify-center">
         <h1
           className={`${bebas_neue.className} lg:text-[90px] xl:text-[101px] text-6xl leading-[90%] `}
@@ -29,25 +36,17 @@ const Hero = () => {
             ))}
           </div>
         </h1>
-        <span className="mt-3 lg:mt-5 xl:w-[40rem] text-base lg:text-lg leading-[150%] text-[#C7C7C7]">
+        <span className="mt-3 lg:mt-5 xl:w-[40rem] text-base lg:text-lg leading-[150%] text-[#C7C7C7] font-[300]">
           An Istanbul-based software engineering student and front-end developer
           passionate about solving problems.
         </span>
 
-        <div className="flex items-center justify-start mt-3 lg:mt-8 gap-5">
-          <button className="bg-primary h-[40px] lg:h-[54px] px-6 pr-16 rounded-[100px] group uppercase text-main-bg relative font-bold">
-            Contact Me
-            <div className="w-3 h-3 rounded-full absolute top-[50%] -translate-y-1/2 group-hover:w-[40px] group-hover:h-[40px] transition-all bg-main-bg right-3 items-center justify-center flex">
-              <BsArrowUpRight className="hidden group-hover:block transition-all opacity-0 group-hover:opacity-100 text-white w-[20px] h-[20px]" />
-            </div>
-          </button>
-          <span className="w-[40px] h-[40px] lg:w-[54px] lg:h-[54px] rounded-full flex items-center justify-center bg-[#222222] cursor-pointer hover:scale-105 transition-all">
-            <FaLinkedinIn className="w-[24px] h-[24px] text-primary" />
-          </span>
-          <span className="w-[40px] h-[40px] lg:w-[54px] lg:h-[54px] rounded-full flex items-center justify-center bg-[#222222] cursor-pointer hover:scale-105 transition-all">
-            <FaGithub className="w-[24px] h-[24px] text-primary" />
-          </span>
-        </div>
+        <Buttons text="Contact Me" onClick={()=>{
+          NProgress.start();
+          router.push('/about');
+          }}
+          Icon={BsArrowUpRight}
+          />
       </div>
 
       <div className="w-full h-[280px] sm:h-[500px] lg:w-[550px] lg:h-[530px] 2xl:w-[600px] 2xl:h-[700px]">
