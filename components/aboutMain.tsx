acac459106100246309cbd4d9abe673e93cff977
fatihbahadir <hunkarhyme@gmail.com>
2024-08-08 10:36:08 +0300

    feat(about): section scale and opacity animations on scroll

--- components/aboutMain.tsx
+++ components/aboutMain.tsx
@@ -1,25 +1,61 @@
+"use client";
 import { bebas_neue } from "@/lib/fonts";
-import React, { ReactNode } from "react";
+import React, { ReactNode, useRef } from "react";
 import Buttons from "./buttons";
 import { FaDownload, FaExpand } from "react-icons/fa";
 import Image from "next/image";
+import { motion, useScroll, useTransform } from "framer-motion";
 
-const AboutMain = ({ title, rightSection, gap, first } : { title:string, rightSection: ReactNode, gap:string , first?: boolean}) => {
+const AboutMain = ({
+  title,
+  rightSection,
+  gap,
+  first,
+}: {
+  title: string;
+  rightSection: ReactNode;
+  gap: string;
+  first?: boolean;
+}) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["0 1", "1 1"],
+  });
+  const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
+  const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.5, 1]);
+
   return (
-    <section className={`h-screen max-w-screen flex items-center justify-center ${first && '-mt-[105px]'} relative`}>
-        <div className="flex lg:flex-row flex-col items-start w-full justify-between lg:justify-between gap-5 lg:gap-0" >
-        <h2
-          className={`${bebas_neue.className} uppercase ${first ? ' lg:text-[90px] xl:text-[101px]'  : 'lg:text-[55px] xl:text-[76px]'} text-6xl leading-[90%] `}
-        >
-          {title}
-        </h2>
+    <motion.div
+    style={{
+      scale: !first ? scaleProgress : 1,
+      opacity: !first ? opacityProgress : 1,
+    }}
+    ref={ref}
+    >
+      <section
+        className={`h-screen max-w-screen flex items-center justify-center ${
+          first && "-mt-[105px]"
+        } relative`}
+      >
+        <div className="flex lg:flex-row flex-col items-start w-full justify-between lg:justify-between gap-5 lg:gap-0">
+          <h2
+            className={`${bebas_neue.className} uppercase ${
+              first
+                ? " lg:text-[90px] xl:text-[101px]"
+                : "lg:text-[55px] xl:text-[76px]"
+            } text-6xl leading-[90%] `}
+          >
+            {title}
+          </h2>
 
-      <div className={`flex flex-col ${gap} max-w-[700px]`}>
-       {rightSection}
-      </div>
-      </div>
-      <div className="absolute w-screen bottom-0 border-b border-[#484848]"></div> 
-    </section>
+          <div className={`flex flex-col ${gap} max-w-[700px]`}>
+            {rightSection}
+          </div>
+        </div>
+        <div className="absolute w-screen bottom-0 border-b border-[#484848]"></div>
+      </section>
+    </motion.div>
   );
 };
 
