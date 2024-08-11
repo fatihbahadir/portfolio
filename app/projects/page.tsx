519afe7132fdb959ad05500bb540da0a54660053
fatihbahadir <hunkarhyme@gmail.com>
2024-08-11 14:54:14 +0300

    feat(projects): current section

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div className='w-full h-screen -mt-[110px] flex items-center justify-center text-2xl lg:text-5xl animate-pulse'>Currently Building 🛠️</div>
   )
 }
 
